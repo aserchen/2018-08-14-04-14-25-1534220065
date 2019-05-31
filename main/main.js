0033478af70ef4fd459e36b51e927bb6fa788a61
aserchen <2446922621@qq.com>
2019-05-31 19:59:14 +0800

    coding

--- main/main.js
+++ main/main.js
@@ -1,4 +1,17 @@
-module.exports = function main() {
+module.exports = function main(str) {
     console.log("Debug Info");
-    return 'Hello World!';
+	var symb=  [['._.','...','._.','._.','...','._.','._.','._.','._.','._.'],
+				['|.|','..|','._|','._|','|_|','|_.','|_.','..|','|_|','|_|'],
+				['|_|','..|','|_.','._|','..|','._|','|_|','..|','|_|','..|']];
+	var reslut=''
+	for(var j=0;j<3;j++){ 
+		for(var i=0;i<str.length;i++){
+			reslut += symb[j][parseInt(str.charAt(i))];
+			if(i!=str.length-1){
+				reslut += ' ';
+			}
+		}
+		reslut += '\n';
+	}
+    return reslut;
 };
